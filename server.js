91d6d1538bd7a072485058a2f4a3d1e8276a484b
Administrator <admin@example.com>
2022-07-22 03:46:16 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World nodejs');
+  res.send('Hello World nodejs haui');
 });
 
 app.listen(PORT, HOST);
